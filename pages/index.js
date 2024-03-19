5ac915aa52db24138e5e1ba151cab44b2f62473d
pedroncios <pedroyamada@gmail.com>
2024-03-19 14:55:52 -0300

    Alterado mensagem impactante

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 function Home() {
-    return <h1>Lyene, te amo meu amor!!! Se você também me ama, da uma risadinha!</h1>
+  const [beijos, setBeijos] = useState("");
+
+  function handleClick() {
+    setBeijos(beijos + "😘");
+  }
+
+  return (
+    <>
+      <h1>🥰 Te amo meu amô linduuuu!!!</h1>
+      <h1>Se você também me ama, da uma risadinha! 🤭</h1>
+      <button onClick={handleClick}>Clique aqui pra ganhar um beijinho</button>
+      <h1>{beijos}</h1>
+    </>
+  );
 }
 
 export default Home;
