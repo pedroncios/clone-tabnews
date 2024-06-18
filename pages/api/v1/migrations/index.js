298a156d129f1c15ac5154ffd4558786b3247433
pedroncios <pedroyamada@gmail.com>
2024-06-18 16:44:56 -0300

    fix db connection leaking

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -37,5 +37,6 @@ export default async function migrations(request, response) {
     return response.status(200).json(migratedMigrations);
   }
 
+  await dbClient.end();
   return response.status(405).end(); // 405 Method Not Allowed
 }
